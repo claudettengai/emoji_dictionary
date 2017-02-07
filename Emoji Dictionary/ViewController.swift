01eeb08f05e2723742fcc0a0f9e93268092dd503
Claudette Ngai <claudettengai@Claudettes-Air.skuldugger>
2017-02-06 17:09:11 -0800

    this is a lovely church emoji

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var coolTableView: UITableView!
+    
+    
+    var emojis = ["🍟", "🍆", "💄", "👙", "🐳", "🐹", "🐙"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableView.dataSource = self
+        coolTableView.delegate = self
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
